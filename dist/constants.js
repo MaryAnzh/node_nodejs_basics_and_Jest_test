2ff3d5f3e362143d97b956c15eb64e16e42e6ef6
MaryAnzh <maryang@mail.ru>
2026-01-22 17:14:22 +0300

    fix: fix gitignore

--- dist/constants.js
+++
@@ -1,9 +0,0 @@
-export const { CREATE_TEXT, ERROR_MESSAGE_FS, FRESH_TXT, SUCCESS, ERROR_MESSAGE, TEST_FOULED, TEST, } = {
-    FRESH_TXT: 'fresh.txt',
-    CREATE_TEXT: 'I am fresh and young',
-    ERROR_MESSAGE_FS: 'FS operation failed',
-    SUCCESS: 'success',
-    ERROR_MESSAGE: 'Ops.. error',
-    TEST_FOULED: 'test fouled',
-    TEST: 'test'
-};
